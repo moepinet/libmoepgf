a5ac5cb7c344b8acfda79c1e1ab057246e98c417
Stephan Guenther <moepi@moepi.net>
2014-03-19 11:13:20 +0100

    added xor.{c,h}

---
+++ src/xor.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014	Stephan M. Guenther <moepi@moepi.net>
+ * 			Maximilian Riemensberger <riemensberger@tum.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * See COPYING for more details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "xor.h"
+
+void
+xorr_scalar(uint8_t *region1, const uint8_t *region2, int length)
+{
+	for(; length; region1++, region2++, length--)
+		*region1 ^= *region2;
+}
+
+void
+xorr_gpr32(uint8_t *region1, const uint8_t *region2, int length)
+{
+	for(; length > 0; region1+=4, region2+=4, length-=4)
+		*(uint32_t *)region1 ^= *(uint32_t *)region2;
+}
+
+void
+xorr_gpr64(uint8_t *region1, const uint8_t *region2, int length)
+{
+	for(; length > 0; region1+=8, region2+=8, length-=8)
+		*(uint64_t *)region1 ^= *(uint64_t *)region2;
+}
---
+++ src/xor.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014	Stephan M. Guenther <moepi@moepi.net>
+ * 			Maximilian Riemensberger <riemensberger@tum.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * See COPYING for more details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _XOR_H_
+#define _XOR_H_
+
+#include <stdint.h>
+
+/*
+ * These functions should not be called directly. Use the function pointers
+ * provided by the galois_field structure instead.
+ */
+void xorr_scalar(uint8_t *region1, const uint8_t *region2, int length);
+void xorr_gpr32(uint8_t *region1, const uint8_t *region2, int length);
+void xorr_gpr64(uint8_t *region1, const uint8_t *region2, int length);
+
+#ifdef __x86_64__
+void xorr_sse2(uint8_t *region1, const uint8_t *region2, int length);
+void xorr_avx2(uint8_t *region1, const uint8_t *region2, int length);
+#endif
+
+#ifdef __arm__
+void xorr_neon_64(uint8_t *region1, const uint8_t *region2, int length);
+void xorr_neon_128(uint8_t *region1, const uint8_t *region2, int length);
+#endif
+
+#endif // _XOR_H_
