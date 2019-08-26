11aa89c66727e78cadb6a1313a712a02703ac609
Stephan Günther <guenther@tum.de>
2019-08-26 10:48:11 +0200

    Fixed built on IBM Power9

--- src/gf.c
+++ src/gf.c
@@ -352,6 +352,12 @@ moepgf_init(struct moepgf *gf, enum MOEPGF_TYPE type, enum MOEPGF_ALGORITHM atyp
 			gf->maddrc = best_algorithms[type][MOEPGF_HWCAPS_SIMD_NEON].maddrc;
 		}
 #endif
+#ifdef __powerpc64__
+		if (hwcaps & (1 << MOEPGF_HWCAPS_SIMD_NONE)) {
+			gf->mulrc  = best_algorithms[type][MOEPGF_HWCAPS_SIMD_NONE].mulrc;
+			gf->maddrc = best_algorithms[type][MOEPGF_HWCAPS_SIMD_NONE].maddrc;
+		}
+#endif
 		else if (hwcaps & (1 << MOEPGF_HWCAPS_SIMD_NONE)) {
 			gf->mulrc  = best_algorithms[type][MOEPGF_HWCAPS_SIMD_NONE].mulrc;
 			gf->maddrc = best_algorithms[type][MOEPGF_HWCAPS_SIMD_NONE].maddrc;
