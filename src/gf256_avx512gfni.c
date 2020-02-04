488ddad76b2c57388c3b24d793d23e999dcbcb88
Julien Schmidt <julien.schmidt@tum.de>
2020-02-04 20:06:15 +0100

    add maddrc256_gfni_avx512 and mulrc256_gfni_avx512 implementations
    Signed-off-by: Stephan Günther <guenther@tum.de>

--- src/gf256_avx512gfni.c
+++ src/gf256_avx512gfni.c
@@ -31,18 +31,56 @@
 #include <moepgf/moepgf.h>
 
 #include "gf256.h"
+#include "xor.h"
 
 void
 maddrc256_gfni_avx512(uint8_t *region1, const uint8_t *region2,
 					uint8_t constant, size_t length)
 {
-	maddrc256_pdiv(region1, region2, constant, length);
+    uint8_t *end;
+    register __m512i in1, in2, out, bc;
+
+	if (constant == 0)
+		return;
+
+	if (constant == 1) {
+		xorr_avx512(region1, region2, length);
+		return;
+	}
+
+    bc = _mm512_set1_epi8(constant);
+
+    for (end=region1+length; region1<end; region1+=64, region2+=64) {
+		in2 = _mm512_load_si512((void *)region2);
+		in1 = _mm512_load_si512((void *)region1);
+		out = _mm512_gf2p8mul_epi8(in2, bc);
+		out = _mm512_xor_si512(out, in1);
+		_mm512_store_si512((void *)region1, out);
+	}
 }
 
 
 void
 mulrc256_gfni_avx512(uint8_t *region, uint8_t constant, size_t length)
 {
-	mulrc256_pdiv(region, constant, length);
+    uint8_t *end;
+    register __m512i in, out, bc;
+
+    if (constant == 0) {
+        memset(region, 0, length);
+        return;
+    }
+
+    if (constant == 1)
+        return;
+
+    bc = _mm512_set1_epi8(constant);
+
+    for (end=region+length; region<end; region+=64) {
+        in = _mm512_load_si512((void *)region);
+        bc = _mm512_set1_epi8(constant);
+        out = _mm512_gf2p8mul_epi8(in, bc);
+        _mm512_store_si512((void *)region, out);
+    }
 }
 
